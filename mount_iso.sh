03001647a40a548e2557685829b326db59960e1e
root <root@lirongjiang.com>
2016-09-12 17:23:12 +0800

    add the code in de file

---
+++ mount_iso.sh
@@ -0,0 +1,10 @@
+#!/bash/bin
+#create date :2016-09-12
+#modify date :2016-09-12
+#author :lirongjiang
+#function : mount iso file on de linux file
+
+
+
+mount -o loop -t iso9660 /mnt/rhel-server-6.5-x86_64-dvd.iso /mnt/cdrom/
+
